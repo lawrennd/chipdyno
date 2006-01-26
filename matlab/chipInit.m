d1c35cf660dcf213f6f1246fe719be95dc0060f9
Neil Lawrence <lawrennd@gmail.com>
2006-01-26 16:54:06 +0000

    *** empty log message ***

--- matlab/chipInit.m
+++
@@ -1,7 +0,0 @@
-function [Sigma, beta, mu]=chipInit(data,X);
-%CHIPINIT initialises the chip parameters
-g=cov(data');
-Sigma=X'*diag(diag(g))*diag(diag(X*X').^(-1))*X;
-%mu=pdinv(Sigma)*X'*mean(data,2);
-mu=zeros(size(X,2),1);
-beta=100;
--- matlab/chipPlotter.m
+++
@@ -1,6 +0,0 @@
-function chipPlotter(TF, TFerror,npts)
-%CHIPPLOTTER plots errorbars in a printable way
-figure, f=errorbar([1:npts],TF,TFerror);
-set(gca,'linewidth',2)
-set(f,'linewidth',2)
-set(gca,'fontsize',15)
